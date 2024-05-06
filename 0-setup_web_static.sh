dfb61db986c3a5809316b0018470ad15a8dd15be
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-07 02:31:50 +0300

    0-setup_web_static.sh_02:31

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,27 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Bash script which sets up a web server for deployment of web_static project
+html_content="
+<html>
+	<head>
+		<style></style>
+	</head>
+	<body>
+		<p>Sample paragraph</p>
+	</body>
+</html>
+"
+nginx_location="#\tlocation alias directive\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}
+"
+sudo apt update -y
+sudo apt-get -y install nginx
+mkdir -p /data/web-static/releases/test
+echo "$html_content" > /data/web-static/releases/test/index.html
 
-apt-get update
-apt-get install -y nginx
+mkdir -p /data/web-static/shared
+mkdir -p /data/web-static/current
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web-static/releases/test /data/web-static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "60a $nginx_location" /etc/nginx/sites-enabled/default
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+sudo service nginx restart
