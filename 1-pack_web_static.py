cb635f8a6946ca5b52ea799de3c6d0eafb8c960b
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-07 06:07:00 -0700

    versions_06:07

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Python script which packs all files in /data/web_static into a compressed file"""
 from fabric.api import run
+from fabric.api import local
 from fabric.api import lcd
 import time
 
@@ -8,8 +9,8 @@ import time
 def do_pack():
     time_component = time.localtime(time.time())
     name_str = "web_static_{}{}{}{}{}{}.tgz".format(time_component.tm_year, time_component.tm_mon, time_component.tm_mday
-                                , time_component.tm_hour, time_component.tm_min, time_component.tm_sec)
-    with lcd("pwd"):
-        res = run("tar -cvzf versions/{} web_static".format(name_str))
-    return (res.stdout)
+                , time_component.tm_hour, time_component.tm_min, time_component.tm_sec)
 
+    local("mkdir -p ./versions/")
+    res = local("tar -cvzf versions/{} web_static".format(name_str))
+    print(res.stdout)
