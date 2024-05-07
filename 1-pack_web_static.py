c49091fc55dbbeb159865f185abeabb1414a1ee4
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-07 16:44:17 +0300

    1-pack_web_static.py_16:44

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,20 @@
 from fabric.api import run
 from fabric.api import local
 from fabric.api import lcd
+from datetime import datetime
 import time
 
 
 def do_pack():
-    time_component = time.localtime(time.time())
+    # time_component = time.localtime(time.time())
+    time_now = datetime.now()
     name_str = "web_static_{}{}{}{}{}{}.tgz".\
-        format(time_component.tm_year, time_component.tm_mon,
-               time_component.tm_mday, time_component.tm_hour,
-               time_component.tm_min, time_component.tm_sec)
+        format(time_now.year, time_now.month,
+               time_now.day, time_now.hour,
+               time_now.minute, time_now.second)
+        # format(time_component.tm_year, time_component.tm_mon,
+        #        time_component.tm_mday, time_component.tm_hour,
+        #        time_component.tm_min, time_component.tm_sec)
 
     local("mkdir -p ./versions/")
     res = local("tar -cvzf versions/{} web_static".format(name_str))
