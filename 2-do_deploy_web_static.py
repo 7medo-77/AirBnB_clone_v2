1d6a110a0aafe30c1f51f8710d15e302e9cd840f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-08 22:30:43 +0300

    __pycache___22:30

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
 """Python script which packs all files in
 /data/web_static into a compressed file"""
-from fabric.api import run, cd, env, put
+from fabric.api import run, cd, env, put, local
+from os import path
 # from fabric.api import cd
 # from fabric.api import env
 # from fabric.api import put
 # from datetime import datetime
 
+
 env.hosts = ['54.82.134.192', '54.146.93.43']
-# env.user = 'ubuntu'
-# env.key_filename = '~/.ssh/school'
+
 
 def do_deploy(archive_path='none'):
     """
     Function to pack files in web_static into a compressed file
     """
-    if (run("test -e {}".format(archive_path)).failed):
-        return False
 
     try:
+        if not (path.exists(archive_path)):
+            return False
+
         with cd("/tmp"):
             put("{}".format(archive_path), "/tmp/")
 
-            output_object = run("ls -t /tmp | awk 'NR==1'")
+            output_object = run("echo $(ls -t /tmp | awk 'NR==1')")
             archive_name = output_object.stdout
-            raw_name = run("echo \"{}\" | cut -d '.' -f 1".format(archive_name)).stdout
+            raw_name = run("echo \"{}\" | cut -d '.' -f 1"
+                           .format(archive_name)).stdout
 
-            run("mkdir -p /data/web_static/releases")
-            run("tar -xvf /tmp/{} -C /data/web_static/releases/{}"\
+            run("mkdir -p /data/web_static/releases/{}".format(raw_name))
+            run("tar -xvf /tmp/{} -C /data/web_static/releases/{}"
                 .format(archive_name, raw_name))
             run("rm -f {}".format(archive_name))
             run("rm -f /data/web_static/current")
-            run("ln -s -f /data/web_static/releases/{} /data/web_static/current".format(raw_name))
+            run("ln -s -f /data/web_static/releases/{}\
+                 /data/web_static/current".format(raw_name))
             return (True)
     except Exception:
         return False
