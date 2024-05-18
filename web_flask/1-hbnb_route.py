ad66717fb0a5e1d9e3cba1ac3160594e2596b3a5
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-18 03:37:56 +0300

    __pycache___03:37

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Flask instance with a routing to root
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """
+    Function which prints the phrase Hello HBNB!
+    """
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes=False)
+def holberton():
+    """
+    Function which prints the phrase HBNB
+    """
+    return ("HBNB")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
