c1234d13cd715c52a19d649247c51a00c046c4f7
hmedo <hmedo@7medo>
2024-06-08 15:03:53 +0300

    3-deploy_web_static.py_15-03

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,8 +12,9 @@ from os import path
 
 
 env.hosts = ['54.82.134.192', '54.146.93.43']
-do_pack = importlib.import_module('./1-pack_web_static.py').do_pack
-do_deploy = importlib.import_module('2-do_deploy_web_static.py').do_deploy
+
+# do_pack = importlib.import_module('./1-pack_web_static.py').do_pack
+# do_deploy = importlib.import_module('2-do_deploy_web_static.py').do_deploy
 
 
 def deploy(archive_path='none'):
