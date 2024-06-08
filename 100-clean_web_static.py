4b287ad3b4baed5d3b76f837e4f3a568c45b4575
hmedo <hmedo@7medo>
2024-06-08 19:43:09 +0300

    100-clean_web_static.py_19-43

---
+++ 100-clean_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Python script which packs all files in
+/data/web_static into a compressed file
+"""
+from fabric.api import (run, cd,
+                        env, put,
+                        local
+                        )
+import importlib
+from os import path
+from datetime import datetime
+
+
+env.hosts = ['54.82.134.192', '54.146.93.43']
+
+
+def do_clean(number=0):
+    """
+    Function to delete older versions of
+    the archive in the versions directory
+    and in the releases directory remotely
+    """
+    numberOfVersions = int(local("ls -t ./versions | wc -l").stdout)
+    listRemoveFiles = local("ls -t ./versions | tail -{}"
+                            .format(numberOfVersions - 1 
+                                    if number <= 1 else numberOfVersions - number)).stdout
+    local("rm -f {}".format(listRemoveFiles))
+
+    with cd("/data/web_static/releases/"):
+        numberOfReleases = int(run("ls -t /data/web_static/releases/ | wc -l").stdout)
+        listRemoveReleases = local("ls -t | tail -{}"
+                                   .format(numberOfReleases - 1 
+                                           if number <= 1 else numberOfReleases - number)).stdout
+        local("rm -f {}".format(listRemoveReleases))
