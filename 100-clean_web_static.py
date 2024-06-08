7d075361eee180f81a7acf8b27b39e7c4630699f
hmedo <hmedo@7medo>
2024-06-08 19:54:35 +0300

    100-clean_web_static.py_19-54

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,12 +4,8 @@ Python script which packs all files in
 /data/web_static into a compressed file
 """
 from fabric.api import (run, cd,
-                        env, put,
-                        local
+                        env, local
                         )
-import importlib
-from os import path
-from datetime import datetime
 
 
 env.hosts = ['54.82.134.192', '54.146.93.43']
@@ -23,13 +19,16 @@ def do_clean(number=0):
     """
     numberOfVersions = int(local("ls -t ./versions | wc -l").stdout)
     listRemoveFiles = local("ls -t ./versions | tail -{}"
-                            .format(numberOfVersions - 1 
-                                    if number <= 1 else numberOfVersions - number)).stdout
+                            .format(numberOfVersions - 1
+                                    if number <= 1
+                                    else numberOfVersions - number)).stdout
     local("rm -f {}".format(listRemoveFiles))
 
     with cd("/data/web_static/releases/"):
-        numberOfReleases = int(run("ls -t /data/web_static/releases/ | wc -l").stdout)
+        numberOfReleases = int(run("ls -t /data/web_static/releases/\
+                            | wc -l").stdout)
         listRemoveReleases = local("ls -t | tail -{}"
-                                   .format(numberOfReleases - 1 
-                                           if number <= 1 else numberOfReleases - number)).stdout
-        local("rm -f {}".format(listRemoveReleases))
+                                   .format(numberOfReleases - 1
+                                           if number <= 1 else
+                                           numberOfReleases - number)).stdout
+        local("rm -rf {}".format(listRemoveReleases))
